571cb931778ba83d12e55ac6aff437f5ac316deb
Farajov Qurban <farajovqurban@users.noreply.github.com>
2019-10-17 00:00:39 +0400

    convertion home try

--- src/main/java/app1.java
+++ src/main/java/app1.java
@@ -2,9 +2,19 @@ public class app1 {
     //psvm
     public static void main(String[] args) {
         //sout
-        System.out.println ("hello there boi");
+        String str = "1050";
 
+        int inum = Integer.parseInt(str);   //return primitive
+        //long ilong = Long.parseLong(longStr);
+        System.out.println(inum);
 
+        Integer onum = Integer.valueOf(str); //return object
+        //Long olong = Long.valueOf(longStr);
+        System.out.println(onum);
+        /*String to boolean
+        A String value can be converted to primitive boolean value using Boolean.parseBoolean
+        method. For conversion to Boolean object, you can use Boolean.valueOf() method.
+         */
 
 
     }
