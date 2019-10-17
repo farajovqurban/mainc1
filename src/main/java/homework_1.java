a4e04afaf8a4d3d7ba2eb1c8c1586b0bd6c57ed2
Farajov Qurban <farajovqurban@users.noreply.github.com>
2019-10-18 01:12:10 +0400

    -pretty much initial

--- src/main/java/homework_1.java
+++ src/main/java/homework_1.java
@@ -3,19 +3,26 @@ import java.util.Scanner;
 
 public class homework_1 {
     public static void main(String[] args) {
-        Random rand1 = new Random();
-        System.out.println("Let the game begin!");
-        System.out.println(rand1);
-        Scanner input = new Scanner(System.in);
 
-        int inum = input.nextInt();
-        int ranum = rand1.nextInt(100);
 
-        if (ranum == inum)
-            System.out.println("Congratulations, {name}!");
-        else if (inum < ranum)
-            System.out.println("Your number is too small. Please, try again.");
-        else
-            System.out.println("Your number is too big. Please, try again.");
+            Random rand1 = new Random();
+            System.out.println("Let the game begin!");
+            System.out.println(rand1);
+            int ranum = rand1.nextInt(100);
+
+
+            Scanner input = new Scanner(System.in);
+            int inum = input.nextInt();
+
+            if (ranum == inum)
+                System.out.println("Congratulations!");
+
+            else if (inum < ranum)
+                System.out.println("Your number is too small. Please, try again.");
+            else
+                System.out.println("Your number is too big. Please, try again.");
+
+
+
     }
 }
