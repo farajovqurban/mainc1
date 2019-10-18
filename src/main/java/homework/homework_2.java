6e09542008f478879df02465eff6803413045960
Farajov Qurban <farajovqurban@users.noreply.github.com>
2019-10-18 22:55:55 +0400

    hw2 added not complete

--- src/main/java/homework/homework_2.java
+++ src/main/java/homework/homework_2.java
@@ -1,23 +1,58 @@
 package homework;
+import java.util.Random;
+import java.util.Scanner;
 
 public class homework_2 {
     public static void main(String[] args) {
         int column = 5;
         int row = 5;
 
-        int [] [] array1 = new int[column][row];
-
+        char [] [] array1 = new char[column][row];
+        System.out.println("All set. Get ready to rumble!");
         for (int i = 0; i < column; i++) {
-
             for (int j = 0; j < row; j++) {
-                System.out.print(array1 [i] [j]);
+                array1 [i] [j] ++;
+                System.out.print("-");
             }
             System.out.println();
         }
 
 
 
+        Random rand = new Random();
+        int ranrow = rand.nextInt(5) + 1;
+        int rancol = rand.nextInt(5) + 1;
+        System.out.println(ranrow + " " + rancol);
 
+            Scanner scn = new Scanner(System.in);
+
+            while(true) {
+                int inrow = scn.nextInt();
+                int incol = scn.nextInt();
+
+                if (inrow == ranrow && incol == rancol) {
+                    System.out.println("You have won!");
+                    break;
+                }
+                else {
+                    System.out.println("try again");
+
+                    for (int i = 0; i < array1.length; i++) {
+                        for (int j = 0; j < row; j++) {
+
+                            array1[i][j] = '-';
+                            array1[inrow][incol] = '*';
+                            System.out.print(array1[i][j]);
+
+                        }
+
+                        System.out.println();
+                    }
+
+                    }
+            }
+
+        }
 
 
 
@@ -30,4 +65,3 @@ public class homework_2 {
 
 
         }
-    }
