6d4b4eb8624e30dd2e54596be358d71c24edff74
Farajov Qurban <farajovqurban@users.noreply.github.com>
2019-10-18 21:27:14 +0400

    -pretty much initial

--- src/main/java/homework/homework_1.java
+++ src/main/java/homework/homework_1.java
@@ -1,27 +1,35 @@
+package homework;
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class homework_1 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean check;
 
+        Random rand1 = new Random();
+        int ranum = rand1.nextInt(100);
 
-            Random rand1 = new Random();
-            System.out.println("Let the game begin!");
-            System.out.println(rand1);
-            int ranum = rand1.nextInt(100);
+        System.out.println("Let the game begin!");
 
-
-            Scanner input = new Scanner(System.in);
+        do {
             int inum = input.nextInt();
+            check = (inum == ranum);
 
-            if (ranum == inum)
+            if (ranum == inum) {
                 System.out.println("Congratulations!");
+                break;
+            }
 
             else if (inum < ranum)
                 System.out.println("Your number is too small. Please, try again.");
+
             else
                 System.out.println("Your number is too big. Please, try again.");
 
+        }
+        while (check = true);
 
 
     }
---
+++ src/main/java/homework/homework_2.java
@@ -0,0 +1,33 @@
+package homework;
+
+public class homework_2 {
+    public static void main(String[] args) {
+        int column = 5;
+        int row = 5;
+
+        int [] [] array1 = new int[column][row];
+
+        for (int i = 0; i < column; i++) {
+
+            for (int j = 0; j < row; j++) {
+                System.out.print(array1 [i] [j]);
+            }
+            System.out.println();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
