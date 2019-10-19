380ad5dbea6b94a7a55d64eb0a0efec2fc636b7a
Farajov Qurban <farajovqurban@users.noreply.github.com>
2019-10-19 13:30:30 +0400

    initial

--- src/main/java/homework/hw1/homework_1.java
+++ src/main/java/homework/hw1/homework_1.java
@@ -1,36 +1,22 @@
 package homework.hw1;
-
 import java.util.Random;
 import java.util.Scanner;
 
 public class homework_1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        boolean check;
-
         Random rand1 = new Random();
         int ranum = rand1.nextInt(100);
-
         System.out.println("Let the game begin!");
 
-        do {
+        while (true){
             int inum = input.nextInt();
-            check = (inum == ranum);
-
             if (ranum == inum) {
                 System.out.println("Congratulations!");
                 break;
             }
-
-            else if (inum < ranum)
-                System.out.println("Your number is too small. Please, try again.");
-
-            else
-                System.out.println("Your number is too big. Please, try again.");
-
+            else if (inum < ranum)  System.out.println("Your number is too small. Please, try again.");
+            else  System.out.println("Your number is too big. Please, try again.");
         }
-        while (check = true);
-
-
     }
 }
